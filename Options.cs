c34a360ff2de13e0465c1986b6cec30f18ae2e47
augus99 <augustocastelo1999@gmail.com>
2021-02-28 10:29:21 -0300

    Update options args default values

--- Options.cs
+++ Options.cs
@@ -3,18 +3,18 @@ using CommandLine;
 namespace Mandelsharp {
     public sealed class Options {
         [Option('s', "size", Required = false, HelpText = "Set result image width and height.")] 
-        public int Size { get; set; }
+        public int Size { get; set; } = 512;
 
         [Option('o', "output", Required = false, HelpText = "Set result image output directory.")] 
-        public string Output { get; set; }
+        public string Output { get; set; } = "./mandelbrot.jpg";
 
         [Option('x', "xcoord", Required = false, HelpText = "Set x coordinate to zoom in.")]
-        public int X { get; set; }
+        public int X { get; set; } = -1;
 
         [Option('y', "ycoord", Required = false, HelpText = "Set y coordinate to zoom in.")]
-        public int Y { get; set; }
+        public int Y { get; set; } = -1;
 
         [Option('z', "zoom", Required = false, HelpText = "Set zoom value.")]
-        public int Zoom { get; set; }
+        public int Zoom { get; set; } = -1;
     }
 }
