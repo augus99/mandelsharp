f4f37ef12a23551bd93cab5ce46208db5150f6af
augus99 <augustocastelo1999@gmail.com>
2021-02-28 10:31:28 -0300

    Add entry point

---
+++ Program.cs
@@ -0,0 +1,24 @@
+using System;
+using CommandLine;
+
+namespace Mandelsharp {
+    public class Program {
+        public static void Main(string[] args) {
+            Parser.Default.ParseArguments<Options>(args).WithParsed<Options>(o => {
+                if(o.Size < 32) Exit("Size must be at least 32px.");
+                else {
+                    // Proccess args
+                }
+            });
+        }
+
+        public static void Exit(string errorMessage) {
+            ConsoleColor prev = Console.ForegroundColor;
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.Write("\nError: ");
+            Console.ForegroundColor = prev;
+            Console.WriteLine(errorMessage);
+            Environment.Exit(-1);
+        }
+    }
+}
