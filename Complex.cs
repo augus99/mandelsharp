82134fe0261e5b949c5dd9024f05ba0b2b79af02
augus99 <augustocastelo1999@gmail.com>
2021-02-28 10:47:44 -0300

    Add Complex class

---
+++ Complex.cs
@@ -0,0 +1,36 @@
+using System;
+
+namespace Mandelsharp {
+    public struct Complex {
+        public double A { get; private set; }
+        public double B { get; private set; }
+        public double Magnitude => Math.Sqrt((A * A) + (B * B));
+
+        public Complex(double a, double b) {
+            this.A = a;
+            this.B = b;
+        }
+
+        public static Complex operator *(Complex left, Complex right) {
+            if(right.B == 0) return left * right.A;
+            if(left.B == 0) return right * left.A;
+
+            double a = left.A * right.A - left.B * right.B;
+            double b = left.A * left.B + right.A * right.B;
+            
+            return new Complex(a, b);
+        }
+
+        public static Complex operator *(Complex left, double right) {
+            return new Complex(left.A * right, left.B * right);
+        }
+
+        public static Complex operator +(Complex left, Complex right) {
+            return new Complex(left.A + right.A, left.B + right.B);
+        }
+
+        public static Complex operator -(Complex left, Complex right) {
+            return new Complex(left.A - right.A, left.B - right.B);
+        }
+    }
+}
